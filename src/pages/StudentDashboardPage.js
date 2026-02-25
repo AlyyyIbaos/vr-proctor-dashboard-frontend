6bfd370996e0896f9f29eb87eeef2cebac3e7d4a
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-02-26 03:19:39 +0800

    revert no badge

--- src/pages/StudentDashboardPage.js
+++ src/pages/StudentDashboardPage.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import StudentLayout from "../component/layout/StudentLayout";
+import StudentLayout from "../components/layout/StudentLayout";
 
 export default function StudentDashboardPage() {
   const navigate = useNavigate();
