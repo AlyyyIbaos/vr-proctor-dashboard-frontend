fc8a24210937169f6fdfc94f0fc3fcd1e89aa46f
Alyssa Denise Ibaos <alyssadeniseibaos@gmail.com>
2026-03-16 17:14:14 +0800

    pt.4

--- src/pages/ProctorExamBuilderPage.js
+++ src/pages/ProctorExamBuilderPage.js
@@ -13,7 +13,6 @@ export default function ProctorExamBuilderPage() {
   ========================= */
 
   const [exams, setExams] = useState([]);
-  const [selectedExam, setSelectedExam] = useState(null);
 
   const fetchExams = async () => {
     try {
